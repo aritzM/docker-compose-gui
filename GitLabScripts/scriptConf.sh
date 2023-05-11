1425934630157a05f7d60f0ffd0b3af87f147325
Aritz <sexquechua@gmail.com>
2023-05-11 22:05:36 +0200

    Separar configuracion de instalacion

--- GitLabScripts/scriptConf.sh
+++ GitLabScripts/scriptConf.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-apt update && apt upgrade -y
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-apt install apache2 mysql-server php php-mysql -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-sudo apt install docker-ce -y
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
-docker compose version
 cat << EOF > /etc/sudoers
 #
 # This file MUST be edited with the 'visudo' command as root.
