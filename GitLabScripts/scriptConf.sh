0f292a33f2680b865adaad74c9ae91d7292fcd7a
Aritz <sexquechua@gmail.com>
2023-05-11 21:42:57 +0200

    Añadir instalacion de apache2, mysql y php

--- GitLabScripts/scriptConf.sh
+++ GitLabScripts/scriptConf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt update && apt upgrade -y
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+apt install apache2 mysql-server php php-mysql -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
