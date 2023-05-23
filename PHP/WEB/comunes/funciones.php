7fcb29d7a23a08540bf4bd1ec6a099dc9b7e2467
Aritz <sexquechua@gmail.com>
2023-05-23 18:36:22 +0200

    Cargar tabla volumenes (actualizarDocker)

--- PHP/WEB/comunes/funciones.php
+++ PHP/WEB/comunes/funciones.php
@@ -181,7 +181,41 @@ function cargarPuertos($data){
   }
 }
 
-function cargarVolumenes($data){
-
+function crearFilaVolumen($data, $countFila){
+  $htmlResult = "";
+  for($i=0; $i < count($data["container"]["services"]); $i++){
+    $htmlResult = $htmlResult . crearColumnaVolumenDServicio($data, $data["container"]["services"][$i], $countFila);
+  }
+  return $htmlResult;
 }
+
+function maxVolumenes($data){
+  $maxResult = 0;
+  for($i=0; $i < count($data["container"]["services"]); $i++){
+    $nombreServicio = $data["container"]["services"][$i];
+    if($maxResult < count($data["container"]["volumes"][$nombreServicio])){
+      $maxResult = count($data["container"]["volumes"][$data["container"]["services"][$i]]);
+    }
+  }
+  return $maxResult;
+}
+
+function cargarFilasVolumenes($data){
+  $countFila = 0;
+  while($countFila < maxVolumenes($data)){
+    $htmlResult = crearFilaVolumen($data, $countFila); 
+    echo "<tr>";
+    echo "<th scope=\"row\">". ($countFila + 1) ."</th>";
+    echo $htmlResult;
+    echo "</tr>";
+    $countFila++;
+  }
+}
+
+function crearColumnaVolumenDServicio($data, $nombreServicio, $filaObtener){
+  $data["container"]["volumes"][$nombreServicio][$filaObtener];
+  $htmlResult = "<td><input type=\"text\" name=\"volumenApache1\" value=\"".$data["container"]["volumes"][$nombreServicio][$filaObtener]."\" class=\"form-control\" placeholder=\"volumen1:volumenDocker\"></td>";
+  return $htmlResult;
+}
+
 ?>
